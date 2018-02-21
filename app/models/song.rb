831ed0d3a2c04341b8103cca25818e6e7376600e
PJ Frias <peejfrias@gmail.com>
2018-02-21 14:50:45 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,15 +3,15 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def artist_name=(artist)
+  def song_artist_name=(artist)
     self.artist = Artist.find_or_create_by(name: artist)
   end
 
-  def artist_name
+  def song_artist_name
     self.artist.name
   end
 
-  def genre_ids=(ids)
+  def song_genre_ids=(ids)
     ids.each do |id|
       self.genres << Genre.find(id)
     end
